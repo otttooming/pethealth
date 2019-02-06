fb2429ffbe77f157ee6ec600fcdfb600f2b1f081
kaarel <kaarel.loide@gmail.com>
2019-02-06 22:46:23 +0200

    Experimenting with landing page design

--- packages/healthvet-front/src/pages/Landing/Landing.tsx
+++ packages/healthvet-front/src/pages/Landing/Landing.tsx
@@ -1,17 +1,73 @@
 import * as React from 'react';
 import { Row, Col, Button } from 'antd';
+import styled from 'styled-components';
+
+const Wrapper = styled.div`
+  background-image: linear-gradient(to left, #ff9176, #ffc093);
+`;
+
+const Image = styled.img``;
+
+const Text = styled.section`
+  font-size: 20px;
+  font-weight: bold;
+  padding-top: 50px;
+  padding-right: 30px;
+  padding-bottom: 50px;
+  padding-left: 80px;
+`;
+
+const Icon = styled.svg`
+  padding-top: 50px;
+  padding-right: 30px;
+  padding-bottom: 50px;
+  padding-left: 80px;
+`;
 
 export interface LandingProps {}
 
 export default class Landing extends React.Component<LandingProps, any> {
   public render() {
     return (
-      <div>
+      <Wrapper>
         <Row gutter={16}>
-          <Col span={12} />
-          <Col span={12} />
+          <Col span={20}>
+            <Row type="flex" justify="start">
+              <Icon>
+                <circle
+                  cx="50"
+                  cy="50"
+                  r="40"
+                  stroke="black"
+                  stroke-width="3"
+                  fill="red"
+                />
+              </Icon>
+            </Row>
+            <Row type="flex" justify="space-around">
+              <Text>
+                With HealthVet we provide solutions that empower your vision and
+                enrich animal healthcare with intuitive collaboration
+              </Text>
+            </Row>
+            <Row type="flex" justify="space-around">
+              <Button
+                type="primary"
+                shape="round"
+                icon="usergroup-add"
+                size="large"
+              >
+                Sign up
+              </Button>
+            </Row>
+          </Col>
+          <Col span={4}>
+            <Row type="flex" justify="space-around">
+              <Image />
+            </Row>
+          </Col>
         </Row>
-      </div>
+      </Wrapper>
     );
   }
 }
