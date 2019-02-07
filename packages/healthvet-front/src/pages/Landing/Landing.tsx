9df47b2f35d49ab2afa5fa4e8afb2df08a2a8b70
kaarel <kaarel.loide@gmail.com>
2019-02-07 23:08:35 +0200

    Adjusted landing page style

--- packages/healthvet-front/src/pages/Landing/Landing.tsx
+++ packages/healthvet-front/src/pages/Landing/Landing.tsx
@@ -13,13 +13,18 @@ const Flex = styled.div`
   display: flex;
 `;
 
+const FlexList = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
 const Image = styled.img`
   position: absolute;
   right: 0;
   top: 0;
-  width: 50%;
+  width: inherit;
   border: none;
-  margin: -1px;
+  margin: -2px;
 `;
 
 const Logo = styled.img`
@@ -40,56 +45,58 @@ const Button1 = styled.button`
   position: absolute;
   background-color: #f57171;
   border: 0;
-  border-radius: 12px;
+  border-radius: 30px;
   color: white;
   width: 400px;
   height: 60px;
   text-align: center;
-  font-size: px;
+  font-size: 26px;
   margin: 4px 2px;
   opacity: 1;
-  transition: 0.3s;
-  display: inline-block;
-  text-decoration: none;
   cursor: pointer;
+
   &:hover {
     opacity: 0.6;
+  }
+`;
+
+const ContentWrapper = styled.div`
+  position: absolute;
+  left: 0;
+  bottom: 0;
+  margin-bottom: 15%;
+  margin-left: 5%;
+  width: 40%
+  color: white;
+  font-size: x-large;
   }
 `;
 
 export interface LandingProps {}
 
 export default class Landing extends React.Component<LandingProps, any> {
-  handleSignIn() {}
-
   public render() {
     return (
       <Flex>
-        <ul
-          style={{
-            listStyleType: 'none',
-            width: '50%',
-            position: 'relative',
-          }}
-        >
-          <li>
-            <Logo src={logo} />
-          </li>
-          <li>
-            <Text>
-              With HealthVet we provide solutions that empower your vision and
-              enrich animal healthcare with intuitive collaboration.
-            </Text>
-          </li>
-          <li>
-            <div style={{ padding: '20px' }}>
-              <Link to="/login">
-                <Button1>Sign up</Button1>
-              </Link>
-            </div>
-          </li>
-        </ul>
-        <Image src={landing_img} />
+        <div>
+          <Logo src={logo} />
+          <FlexList>
+            <ContentWrapper>
+              <Text>
+                With HealthVet we provide solutions that empower your vision and
+                enrich animal healthcare with intuitive collaboration.
+              </Text>
+              <div style={{ padding: '20px' }}>
+                <Link to="/login">
+                  <Button1>Sign up</Button1>
+                </Link>
+              </div>
+            </ContentWrapper>
+          </FlexList>
+        </div>
+        <div style={{ width: '55%' }}>
+          <Image src={landing_img} />
+        </div>
       </Flex>
     );
   }
