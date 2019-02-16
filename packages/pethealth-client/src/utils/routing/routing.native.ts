9f8b649d2474140c23f4645897cf3fb4fdcd95f3
Ott Tooming <OttTooming@gmail.com>
2019-02-16 11:26:13 +0200

    Add platform specific routing using react-router

---
+++ packages/pethealth-client/src/utils/routing/routing.native.ts
@@ -0,0 +1,6 @@
+import * as RouterPackage from 'react-router-native';
+
+export const Link = RouterPackage.Link;
+export const Router = RouterPackage.NativeRouter;
+
+export default RouterPackage;
---
+++ packages/pethealth-client/src/utils/routing/routing.ts
@@ -0,0 +1,7 @@
+import * as RouterPackage from 'react-router-dom';
+
+export const Router = RouterPackage.BrowserRouter;
+
+export const Link = RouterPackage.Link;
+
+export default RouterPackage;
