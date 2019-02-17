4dd17f5fade3f2acf4b743f165575f3cb6fdbfbc
Ott Tooming <OttTooming@gmail.com>
2019-02-17 16:08:08 +0200

    Add Grid for Dashboard view

--- packages/pethealth-client/src/views/Dashboard/Dashboard.tsx
+++ packages/pethealth-client/src/views/Dashboard/Dashboard.tsx
@@ -3,17 +3,27 @@ import RX from 'reactxp';
 import Avatar from '../../components/Avatar';
 import Icon, { IconName } from '../../components/Icon';
 import Card from '../../components/Card';
+import Grid, { Col } from '../../components/Grid';
 
 class Dashboard extends RX.Component {
   public render() {
     return (
-      <RX.View style={{ overflow: 'visible' }}>
-        <RX.Text>Dashboard view</RX.Text>
-        <Avatar />
-        <Icon name={IconName.PLUS} />
+      <Grid>
+        <Col>
+          <RX.Text>Dashboard view</RX.Text>
+          <Avatar />
+          <Icon name={IconName.PLUS} />
+        </Col>
 
-        <Card name="Roboto" date="2019/02/16" />
-      </RX.View>
+        <Col>
+          <Card
+            name="Roboto"
+            date="2019/02/16"
+            description="Cats can get infected by tapeworms, hookworms and whipworms. They get tapeworms by ingesting fleas infected with tapeworm eggs or by eating infected rodents. “The entire worm matures in the large intestine of the cat,” Lund said. “[Then] pieces of th..."
+          />
+        </Col>
+        <Col>Third</Col>
+      </Grid>
     );
   }
 }
