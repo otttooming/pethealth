86f65b62e6d262f34f9f6e36c1b2d434435875f0
Ott Tooming <OttTooming@gmail.com>
2019-02-17 17:19:13 +0200

    Add port of FeedSection for React Native

--- packages/pethealth-client/src/views/Dashboard/Dashboard.tsx
+++ packages/pethealth-client/src/views/Dashboard/Dashboard.tsx
@@ -2,14 +2,14 @@ import React from 'react';
 import RX from 'reactxp';
 import Avatar from '../../components/Avatar';
 import Icon, { IconName } from '../../components/Icon';
-import Card from '../../components/Card';
 import Grid, { Col } from '../../components/Grid';
+import FeedSection from './FeedSection';
 
 const style = {
   wrapper: RX.Styles.createViewStyle({
     overflow: 'visible',
     flex: 1,
-    backgroundColor: '#ffc093',
+    backgroundColor: '#ffe7d6',
   }),
   left: RX.Styles.createViewStyle({
     padding: 32,
@@ -36,11 +36,7 @@ class Dashboard extends RX.Component {
           </Col>
 
           <Col>
-            <Card
-              name="Roboto"
-              date="2019/02/16"
-              description="Cats can get infected by tapeworms, hookworms and whipworms. They get tapeworms by ingesting fleas infected with tapeworm eggs or by eating infected rodents. “The entire worm matures in the large intestine of the cat,” Lund said. “[Then] pieces of th..."
-            />
+            <FeedSection />
           </Col>
           <Col>Third</Col>
         </Grid>
---
+++ packages/pethealth-client/src/views/Dashboard/FeedSection.tsx
@@ -0,0 +1,85 @@
+import * as React from 'react';
+import RX from 'reactxp';
+import Card from '../../components/Card';
+import { Link } from '../../utils/routing/routing';
+import { CardProps } from '../../components/Card/Card';
+
+export interface FeedSectionProps {}
+
+export interface FeedSectionState {
+  patients: CardProps[];
+}
+
+const style = {
+  wrapper: RX.Styles.createViewStyle({
+    overflow: 'visible',
+    flex: 1,
+  }),
+  listItem: RX.Styles.createViewStyle({
+    paddingBottom: 16,
+    paddingTop: 16,
+    paddingRight: 32,
+    paddingLeft: 32,
+  }),
+  list: RX.Styles.createViewStyle({
+    overflow: 'visible',
+  }),
+};
+
+export default class FeedSection extends RX.Component<FeedSectionProps, any> {
+  patients: CardProps[] = [
+    {
+      //   title: 'Mr Wluffy / Worms',
+      description: `Cats can get infected by tapeworms, hookworms and whipworms. They get tapeworms by ingesting fleas infected with tapeworm eggs or by eating infected rodents. “The entire worm matures in the large intestine of the cat,” Lund said. “[Then] pieces of th...`,
+      date: '24.02.2019',
+      name: 'Dr. Elina',
+    },
+    {
+      //   title: 'Max / Gastointestinal problems',
+      description: `Gastrointestinal (GI) disorders and diseases affect a dog's stomach and intestines, resulting in pain and other problems. Any disorder that reduces the digestion or absorption of food, or alters its passage through the digestive tract, can be called a digestive disord... `,
+      date: '24.02.2019',
+      name: 'Dr. Bergman',
+    },
+    {
+      //   title: 'Captain Rio / Psittacosis',
+      description: `Psittacosis, or "Parrot Fever," is a form of the Chlamydia bacterium that can affect all hookbills. The disease is highly contagious and can be passed from birds to other animals, as well as humans. Symptoms of Psittacosis include difficulty in breathing, eye inf...`,
+      date: '24.02.2019',
+      name: 'Dr. House',
+    },
+    {
+      //   title: 'Mr Wluffy / Worms',
+      description: `Dogs are sensitive to fleas and can develop allergies causing serious skin disease. Flea allergies can be quite severe, even if fleas are not seen on your dog. Pets with flea allergies are allergic to the flea saliva and become very itchy after being bitten. Use of flea pro...`,
+      date: '24.02.2019',
+      name: 'Dr. Williams',
+    },
+  ];
+
+  state = {
+    patients: this.patients,
+  };
+
+  renderCardItems = () => {
+    const { patients } = this.state;
+
+    return patients.map((item, index) => (
+      <RX.View style={style.listItem} key={index}>
+        {' '}
+        <Link to="/detail" style={{ textDecoration: 'none' }}>
+          <Card {...item} />
+        </Link>
+      </RX.View>
+    ));
+  };
+
+  public render() {
+    return (
+      <RX.View style={style.wrapper}>
+        {/* <FilterSection /> */}
+
+        <RX.ScrollView style={style.list}>
+          {this.renderCardItems()}
+        </RX.ScrollView>
+      </RX.View>
+    );
+  }
+}
