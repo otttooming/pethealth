31ffe310fc5fb0010c40af7d79da00ea07f4748a
Ott Tooming <OttTooming@gmail.com>
2019-02-17 16:01:10 +0200

    Add Description component for Card

--- packages/pethealth-client/src/components/Card/Card.tsx
+++ packages/pethealth-client/src/components/Card/Card.tsx
@@ -3,10 +3,13 @@ import * as React from 'react';
 import Avatar from '../Avatar';
 import Name from './components/Name';
 import Title from './components/Title';
+import Icon, { IconName } from '../Icon';
+import Description from './components/Description';
 
 export interface CardProps {
   name: string;
   date: string;
+  description: string;
 }
 
 const style = {
@@ -30,8 +33,13 @@ const style = {
   }),
   center: RX.Styles.createViewStyle({
     alignSelf: 'stretch',
+    flex: 1,
     padding: 24,
     paddingRight: 88,
+  }),
+  right: RX.Styles.createViewStyle({
+    alignSelf: 'stretch',
+    padding: 32,
   }),
 };
 
@@ -39,10 +47,11 @@ class Card extends RX.Component<CardProps, any> {
   static defaultProps = {
     name: null,
     date: null,
+    description: null,
   };
 
   public render() {
-    const { name, date } = this.props;
+    const { name, date, description } = this.props;
 
     return (
       <RX.View style={style.wrapper}>
@@ -52,6 +61,12 @@ class Card extends RX.Component<CardProps, any> {
         </RX.View>
         <RX.View style={style.center}>
           <Title title={name} date={date} />
+          <Description description={description} />
+        </RX.View>
+        <RX.View style={style.right}>
+          <Icon name={IconName.PLUS} />
+          <Icon name={IconName.PLUS} />
+          <Icon name={IconName.PLUS} />
         </RX.View>
       </RX.View>
     );
