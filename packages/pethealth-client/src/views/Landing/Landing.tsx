cca1f387e830850a30d358b7c68d366f7e73e1ee
Ott Tooming <OttTooming@gmail.com>
2019-02-20 01:22:24 +0200

    Add responsive adjustments for Landing

--- packages/pethealth-client/src/views/Landing/Landing.tsx
+++ packages/pethealth-client/src/views/Landing/Landing.tsx
@@ -1,32 +1,62 @@
 import React from 'react';
 import RX from 'reactxp';
 import { Link } from '../../utils/routing/routing';
+import Button from '../../components/Button';
+import { ButtonType } from '../../components/Button/Button';
 
-const myViewStyle = RX.Styles.createViewStyle({
-  backgroundColor: '#fff',
-  shadowOpacity: 0.75,
-  shadowRadius: 5,
-  shadowColor: 'red',
-  shadowOffset: { height: 0, width: 0 },
-  // alignSelf: 'stretch',
-});
+const style = {
+  wrapper: RX.Styles.createViewStyle({
+    alignItems: 'center',
+    alignSelf: 'stretch',
+    justifyContent: 'center',
+    backgroundColor: 'rgb(255, 109, 109)',
+    height: '100%' as any,
+  }),
+  footer: RX.Styles.createViewStyle({
+    paddingTop: 32,
+  }),
+  title: RX.Styles.createTextStyle({
+    fontSize: 32,
+    color: '#fff',
+    marginTop: 32,
+  }),
+  description: RX.Styles.createTextStyle({
+    fontSize: 16,
+    color: '#fff',
+    marginTop: 8,
+  }),
+  image: RX.Styles.createImageStyle({
+    alignItems: 'center',
+    height: 256,
+    width: 256,
+    marginTop: 64,
+  }),
+};
 
 class Landing extends RX.Component {
   public render() {
     return (
-      <RX.ScrollView style={myViewStyle}>
-        <RX.ScrollView>
-          <RX.Text selectable={true}>Landing view</RX.Text>
-          <RX.Text>ABC</RX.Text>
-        </RX.ScrollView>
+      <RX.ScrollView>
+        <RX.View style={style.wrapper}>
+          <RX.Image
+            style={style.image}
+            source="https://res.cloudinary.com/dqa53guw9/image/upload/v1550518304/logo-valge.png"
+          />
+          <RX.Text selectable={true} style={style.title}>
+            Protecting all nine lives
+          </RX.Text>
 
-        <RX.ScrollView>
-          <RX.Text selectable={true}>Landing view</RX.Text>
-          <RX.Text>ABC</RX.Text>
-          <Link to="/login">
-            <RX.Text>Link to login</RX.Text>
-          </Link>
-        </RX.ScrollView>
+          <RX.Text selectable={true} style={style.description}>
+            Providing solutions that empower your vision and improve animal
+            healthcare with intuitive collaboration.
+          </RX.Text>
+
+          <RX.View style={style.footer}>
+            <Link to="/login">
+              <Button type={ButtonType.WHITE}>Link to login</Button>
+            </Link>
+          </RX.View>
+        </RX.View>
       </RX.ScrollView>
     );
   }
