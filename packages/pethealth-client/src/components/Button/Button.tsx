de2db933ebec7b028cfe31e3a572d61d3237d391
Ott Tooming <OttTooming@gmail.com>
2019-02-20 01:21:42 +0200

    Add ButtonType defined style variations

--- packages/pethealth-client/src/components/Button/Button.tsx
+++ packages/pethealth-client/src/components/Button/Button.tsx
@@ -1,7 +1,13 @@
 import * as React from 'react';
 import RX from 'reactxp';
 
-export interface ButtonProps {}
+export enum ButtonType {
+  WHITE = 'white',
+  RED = 'red',
+}
+export interface ButtonProps {
+  type: ButtonType;
+}
 
 const style = {
   wrapper: RX.Styles.createButtonStyle({
@@ -24,15 +30,53 @@ const style = {
 };
 
 class Button extends RX.Component<ButtonProps, any> {
+  static defaultProps = {
+    type: ButtonType.RED,
+  };
+
   public render() {
     const { children } = this.props;
 
     return (
-      <RX.Button style={style.wrapper}>
-        <RX.Text style={style.text}>{children}</RX.Text>
+      <RX.Button style={this.getWrapperStyle()}>
+        <RX.Text selectable={true} style={this.getButtonStyle()}>
+          {children}
+        </RX.Text>
       </RX.Button>
     );
   }
+
+  private getWrapperStyle = () => {
+    const { type } = this.props;
+
+    const backgroundColor = type === ButtonType.WHITE ? '#fff' : '#ff6d6d';
+
+    return RX.Styles.createButtonStyle({
+      overflow: 'visible',
+      backgroundColor,
+      borderRadius: 90,
+    });
+  };
+
+  private getButtonStyle = () => {
+    const { type } = this.props;
+
+    const color = type === ButtonType.WHITE ? '#333' : '#fff';
+
+    return RX.Styles.createTextStyle({
+      textDecorationLine: 'none',
+      fontStyle: 'normal',
+      lineHeight: 22,
+      fontSize: 17,
+      textAlign: 'center',
+      color,
+      paddingTop: 8,
+      paddingBottom: 8,
+      paddingRight: 40,
+      paddingLeft: 40,
+      borderWidth: 0,
+    });
+  };
 }
 
 export default Button;
