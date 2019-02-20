87e738d6c6ebb3d2f54b06e4412cf7433221c993
Ott Tooming <OttTooming@gmail.com>
2019-02-21 01:23:07 +0200

    Fix token retrival from Storage not handling a Promise

--- packages/pethealth-client/src/utils/withApollo/client.ts
+++ packages/pethealth-client/src/utils/withApollo/client.ts
@@ -4,8 +4,8 @@ import { createHttpLink } from 'apollo-link-http';
 import { InMemoryCache } from 'apollo-cache-inmemory';
 import { setContext } from 'apollo-link-context';
 
-const authLink = setContext((_, { headers }) => {
-  const token = Storage.getItem('token');
+const authLink = setContext(async (_, { headers }) => {
+  const token = await Storage.getItem('token');
 
   return {
     headers: {
