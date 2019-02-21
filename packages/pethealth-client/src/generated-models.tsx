5529d8d8201cddf061786bbf9b8ab3cb528b79db
Ott Tooming <OttTooming@gmail.com>
2019-02-21 02:28:24 +0200

    Add dashboardList query

--- packages/pethealth-client/src/generated-models.tsx
+++ packages/pethealth-client/src/generated-models.tsx
@@ -23,6 +23,32 @@ export type AuthLoginLogin = {
   token: string;
 };
 
+export type DashboardListVariables = {};
+
+export type DashboardListQuery = {
+  __typename?: 'Query';
+
+  feed: DashboardListFeed[];
+};
+
+export type DashboardListFeed = {
+  __typename?: 'Post';
+
+  id: string;
+
+  content: Maybe<string>;
+
+  createdAt: DateTime;
+
+  author: DashboardListAuthor;
+};
+
+export type DashboardListAuthor = {
+  __typename?: 'User';
+
+  name: Maybe<string>;
+};
+
 import * as ReactApollo from 'react-apollo';
 import * as React from 'react';
 
@@ -75,4 +101,49 @@ export function AuthLoginHOC<TProps, TChildProps = any>(
     AuthLoginVariables,
     AuthLoginProps<TChildProps>
   >(AuthLoginDocument, operationOptions);
+}
+export const DashboardListDocument = gql`
+  query dashboardList {
+    feed {
+      id
+      content
+      createdAt
+      author {
+        name
+      }
+    }
+  }
+`;
+export class DashboardListComponent extends React.Component<
+  Partial<ReactApollo.QueryProps<DashboardListQuery, DashboardListVariables>>
+> {
+  render() {
+    return (
+      <ReactApollo.Query<DashboardListQuery, DashboardListVariables>
+        query={DashboardListDocument}
+        {...(this as any)['props'] as any}
+      />
+    );
+  }
+}
+export type DashboardListProps<TChildProps = any> = Partial<
+  ReactApollo.DataProps<DashboardListQuery, DashboardListVariables>
+> &
+  TChildProps;
+export function DashboardListHOC<TProps, TChildProps = any>(
+  operationOptions:
+    | ReactApollo.OperationOption<
+        TProps,
+        DashboardListQuery,
+        DashboardListVariables,
+        DashboardListProps<TChildProps>
+      >
+    | undefined,
+) {
+  return ReactApollo.graphql<
+    TProps,
+    DashboardListQuery,
+    DashboardListVariables,
+    DashboardListProps<TChildProps>
+  >(DashboardListDocument, operationOptions);
 }
