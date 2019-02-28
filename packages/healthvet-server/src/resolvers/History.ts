bd81f4a31295d8d14c100e4fa61df61cc017f66a
Ott Tooming <OttTooming@gmail.com>
2019-02-28 18:33:22 +0200

    Add author resolver for History

--- packages/healthvet-server/src/resolvers/History.ts
+++ packages/healthvet-server/src/resolvers/History.ts
@@ -3,8 +3,8 @@ import { HistoryResolvers } from '../generated/graphqlgen';
 export const History: HistoryResolvers.Type = {
   ...HistoryResolvers.defaultResolvers,
 
-  author: (parent, args, ctx) => {
-    throw new Error('Resolver not implemented');
+  author: ({ id }, args, ctx) => {
+    return ctx.prisma.history({ id }).author();
   },
   post: ({ id }, args, ctx) => {
     return ctx.prisma.history({ id }).post();
